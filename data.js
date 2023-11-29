fdfb049b9cf525f7558d63cecd4ee57267939c2c
19StRoNGeR98 <stasenko@kvantoriumpskov.ru>
2023-11-29 14:41:54 +0300

    Starboom

---
+++ data.js
@@ -0,0 +1,455 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Улица",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -1.8495179662685146,
+        "pitch": 0.1171114831588067,
+        "fov": 1.405674899209756
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.932265630677371,
+          "pitch": 0.5526140023559183,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Улица)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 2.1461138222202365,
+        "pitch": 0.17237709619361397,
+        "fov": 1.405674899209756
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0579081117021243,
+          "pitch": 0.6468997280717073,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.47210605714787945,
+          "pitch": 0.567272820706453,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 2.9642012021178736,
+        "pitch": -0.0017463595243860652,
+        "fov": 1.405674899209756
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4362942659779794,
+          "pitch": 0.9493507352088084,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.776986083890379,
+          "pitch": 0.7649147922648929,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "Место встречи",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3151499320152062,
+          "pitch": 0.901882094769551,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.12944263919024124,
+          "pitch": 1.0089613952723617,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0421003545979861,
+          "pitch": 0.41511095862867187,
+          "title": "Место встречи",
+          "text": "<span style=\"color: rgb(0, 0, 0); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; font-size: 16px; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">Наши приветливые администраторы всегда рады  ученикам и новым гостям! Ответим на все вопросы и поможем родителям в процессе обучения детей </span>"
+        },
+        {
+          "yaw": -1.1229816496545428,
+          "pitch": 0.022676286474590412,
+          "title": "Информационный стенд",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "Перекрёсток",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 1.5750941606786188,
+        "pitch": 0.1613466857055652,
+        "fov": 1.405674899209756
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.321822984615256,
+          "pitch": 1.0775855598931088,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": 1.691625156335311,
+          "pitch": 0.9699640350270862,
+          "rotation": 0,
+          "target": "5--"
+        },
+        {
+          "yaw": 2.685106857858143,
+          "pitch": 0.9127131331107812,
+          "rotation": 0,
+          "target": "6--"
+        },
+        {
+          "yaw": 0.4312526604716638,
+          "pitch": 0.8978356034551265,
+          "rotation": 0,
+          "target": "7--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5594193966807008,
+          "pitch": 0.25206752910356656,
+          "title": "Лестница успеха",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "5--",
+      "name": "Зона отдыха",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 1.7585982390588448,
+        "pitch": -0.07524709819702657,
+        "fov": 1.405674899209756
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9323340823664843,
+          "pitch": 0.920898054969749,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8274834002926443,
+          "pitch": -0.04020150914044329,
+          "title": "Фотозона",
+          "text": ""
+        },
+        {
+          "yaw": -0.6102502906764293,
+          "pitch": 0.6802315576537907,
+          "title": "Место отдыха",
+          "text": "<span style=\"color: rgb(0, 0, 0); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; font-size: 16px; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">Кто хорошо учится - тот хорошо отдыхает!  </span>"
+        },
+        {
+          "yaw": 0.7057117306682485,
+          "pitch": 0.16151507001877619,
+          "title": "<span style=\"color: rgb(0, 0, 0); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">СУВЕНИРный шкаф</span>",
+          "text": "<span style=\"color: rgb(0, 0, 0); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; font-size: 16px; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">Витрина, в которой представлен весь ассортимент подарков, которые ребята могут заработать своими собственными стараниями и усилиями</span>"
+        }
+      ]
+    },
+    {
+      "id": "6--",
+      "name": "Классная комната",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -2.6534128313549914,
+        "pitch": -0.07801120966151665,
+        "fov": 1.405674899209756
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.671180649810095,
+          "pitch": 0.4369113241838001,
+          "rotation": 5.497787143782138,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.686614663662631,
+          "pitch": -0.002653011205238087,
+          "title": "<span style=\"color: rgb(0, 0, 0); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">КЛАССНАЯ комната</span>",
+          "text": "<span style=\"color: rgb(0, 0, 0); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; font-size: 16px; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">Учиться будет легче! Здесь проходят занятия по ментальной арифметике, эффективному чтению, коррекции почерка. </span>"
+        }
+      ]
+    },
+    {
+      "id": "7--",
+      "name": "Лестница успеха",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0042554760441096,
+          "pitch": 0.9331824419693717,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.07186359631826278,
+          "pitch": 0.8777287786569339,
+          "rotation": 0,
+          "target": "8--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5931570904860752,
+          "pitch": 0.19833230631651588,
+          "title": "<span style=\"color: rgb(0, 0, 0); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">Туалетная комната</span>",
+          "text": "<span style=\"color: rgb(0, 0, 0); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; font-size: 16px; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">Место гигиены, где можно привести себя в порядок и подготовиться к занятию </span>"
+        }
+      ]
+    },
+    {
+      "id": "8--",
+      "name": "Компьютерный клас",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 2.7588752935739036,
+        "pitch": 0.30498546012393213,
+        "fov": 1.405674899209756
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7205187462573548,
+          "pitch": 0.7691914167825349,
+          "rotation": 0,
+          "target": "7--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9834448988698767,
+          "pitch": 0.034600842628876194,
+          "title": "<span style=\"color: rgb(0, 0, 0); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">ЗаКОДированный класс</span>",
+          "text": "<span style=\"color: rgb(0, 0, 0); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; font-size: 16px; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">Здесь проходят занятия по программированию - создаем игры, виртуальные миры, сайты, компьютерные программы </span><span style=\"color: rgb(0, 0, 0); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; font-size: 16px; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">\n</span><br>"
+        }
+      ]
+    }
+  ],
+  "name": "StarBoom",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
